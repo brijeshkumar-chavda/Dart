819524c98f816a9a96b42c8cee45bcb37cc145d7
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-08-03 17:20:04 +0530

    add: main function

---
+++ 03_main_function.dart
@@ -0,0 +1,8 @@
+// WHAT
+// This function is the entry point for the dart program.
+// It doesn't return the value but its work is to execute the user statement and user function.
+
+// NOTE
+// Dart file can have only one main function.
+
+void main() {}
