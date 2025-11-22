8ca1d6a0a16f01dc19118af31ba102c4c1bc6aac
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-22 20:40:03 +0530

    refactor: implicit type

--- 04_datatype/02_implicit_type/04_late.dart
+++ 04_datatype/02_implicit_type/04_late.dart
@@ -3,7 +3,7 @@
 // What
 // - The late keyword in Dart is a declarator used to signal to the compiler that a non-nullable variable will be initialized at a time after its declaration. It is a promise to the Dart compiler that the variable will have a value before it is first accessed (read).
 
-// NOTE
+// Note
 // - Can be initialized later
 // - Can be reassign (same type value)
 // - Can't be redeclare
