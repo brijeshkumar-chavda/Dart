b57aea37c2cc0c63183f3043fbbfcc0699d0a50e
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-22 20:13:32 +0530

    refactor: variable

--- 03_variable/variable.dart
+++ 03_variable/variable.dart
@@ -1,12 +1,10 @@
-// Rules for creating variable
-
 // Rules For Creating Variables In Dart
-// Variable names are case sensitive, i.e., a and A are different.
-// A variable name can consist of letters and alphabets.
-// A variable name cannot start with a number.
-// Keywords are not allowed to be used as a variable name.
-// Blank spaces are not allowed in a variable name.
-// Special characters are not allowed except for the underscore (_) and the dollar ($) sign.
+// - Variable names are case sensitive, i.e., a and A are different.
+// - A variable name can consist of letters and alphabets.
+// - A variable name cannot start with a number.
+// - Keywords are not allowed to be used as a variable name.
+// - Blank spaces are not allowed in a variable name.
+// - Special characters are not allowed except for the underscore (_) and the dollar ($) sign.
 
 void main() {
   var variableName = "value";
