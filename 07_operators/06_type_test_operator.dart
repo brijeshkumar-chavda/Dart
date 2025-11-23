9f5a9bd81dbac9e213c56b3581d3d34c7bdc156b
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-23 18:13:19 +0530

    add: type test operator

---
+++ 07_operators/06_type_test_operator.dart
@@ -0,0 +1,8 @@
+// Type test operators
+
+void main() {
+  var variable = "A string value";
+
+  print(variable is String); // Checks if variable is of type String
+  print(variable is! int); // Checks if variable is not of type int
+}
