19fd789a7ef0e6aff88b1b35d994877351cfbc5d
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-23 18:49:08 +0530

    add: .toUpperCase()

---
+++ string/method/toUpperCase.dart
@@ -0,0 +1,17 @@
+// .toUpperCase()
+
+// What
+// - It is a method of the String class in Dart.
+// - It converts all the characters in the string to uppercase and returns a new string.
+
+// Syntax
+// variable.toUpperCase();
+
+// Example
+void main() {
+  String myString = "Hello World!";
+  String upperCaseString = myString.toUpperCase();
+
+  print("Original String: $myString");
+  print("Uppercase String: $upperCaseString");
+}
