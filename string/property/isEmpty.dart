e4321aaa186bec69f1e8ab6a1b1bd51e3e588b94
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-23 18:44:18 +0530

    add: .isEmpty

---
+++ string/property/isEmpty.dart
@@ -0,0 +1,17 @@
+// .isEmpty
+
+// What
+// - It is a property of the String class in Dart.
+// - It checks whether the string is empty or not and returns a boolean value.
+
+// Syntax
+// variable.isEmpty;
+
+// Example
+void main() {
+  String myString = "";
+  bool isEmpty = myString.isEmpty;
+
+  print("String: $myString");
+  print("Is Empty: $isEmpty");
+}
