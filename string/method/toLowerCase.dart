57763348172d49d7271f2b33a5558b6fd2445607
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-23 18:48:13 +0530

    add: .toLowerCase()

---
+++ string/method/toLowerCase.dart
@@ -0,0 +1,17 @@
+// .toLowerCase()
+
+// What
+// - It is a method of the String class in Dart.
+// - It converts all the characters in the string to lowercase and returns a new string.
+
+// Syntax
+// variable.toLowerCase();
+
+// Example
+void main() {
+  String myString = "Hello World!";
+  String lowerCaseString = myString.toLowerCase();
+
+  print("Original String: $myString");
+  print("Lowercase String: $lowerCaseString");
+}
