fc63b04f79bac69478a1249efd7e7091abf29f6b
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-23 18:09:07 +0530

    add: relational operator

---
+++ 07_operators/04_relational_operators.dart
@@ -0,0 +1,13 @@
+// Relation operators
+
+void main() {
+  int a = 10;
+  int b = 20;
+
+  print(a == b); // Equal to
+  print(a != b); // Not equal to
+  print(a > b); // Greater than
+  print(a < b); // Less than
+  print(a >= b); // Greater than or equal to
+  print(a <= b); // Less than or equal to
+}
