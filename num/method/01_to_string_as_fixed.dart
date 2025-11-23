ca1e7160aec0b43affd7c5b327586f17f8a423f6
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-23 13:26:28 +0530

    add: .toStringAsFixed

---
+++ num/method/01_to_string_as_fixed.dart
@@ -0,0 +1,17 @@
+// .toStringAsFixed()
+
+// What
+// - It is method for number which applicable for int and double.
+// - Converts a number to a string representation with a fixed number of decimal places.
+
+// Syntax
+// variable.toStringAsFixed(precision digit);
+
+// Note
+// - This will return string.
+
+// Example
+void main() {
+  num variableName = 99.126;
+  print(variableName.toStringAsFixed(2));
+}
--- string/01_string_interpolation.dart
+++ string/01_string_interpolation.dart
@@ -1,6 +1,9 @@
+// String interpolation
+
 void main() {
   String variable1 = "value1";
   String variable2 = "value2";
 
   print("I'm $variable1 and I'm $variable2");
 }
+
