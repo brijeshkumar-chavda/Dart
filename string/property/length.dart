0e85d61c0883d1979b03282bda0a1fb99becc2d2
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-23 18:46:15 +0530

    add: length

---
+++ string/property/length.dart
@@ -0,0 +1,17 @@
+// .length
+
+// What
+// - It is a property of the String class in Dart.
+// - It returns the number of characters in the string.
+
+// Syntax
+// variable.length;
+
+// Example
+void main() {
+  String myString = "Hello";
+  int length = myString.length; // 5
+
+  print("String: $myString");
+  print("Length: $length");
+}
