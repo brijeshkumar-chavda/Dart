699ee21a4024abc0074dc3d7859f7b7d029faa95
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-25 14:05:44 +0530

    add: else if statement

---
+++ 09_condition/02_else_if.dart
@@ -0,0 +1,16 @@
+// else if condition statement
+
+// What
+// - if first condition is false then second condition which is else if will be evaluate.
+// - if both condition are fail to evaluate then else block execute.
+
+// Example
+void main() {
+  if (10 > 0) {
+    print("if block executed");
+  } else if (20 > 0) {
+    print("if block executed");
+  } else {
+    print("else block executed");
+  }
+}
