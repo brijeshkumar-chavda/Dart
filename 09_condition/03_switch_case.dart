5e6e03af558dea3732a453279e639f949e246e3f
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-25 14:13:49 +0530

    add: switch case

---
+++ 09_condition/03_switch_case.dart
@@ -0,0 +1,27 @@
+// Switch case statement
+
+void main() {
+  String caseType = "case 1";
+
+  switch (caseType) {
+    case "case 1":
+      {
+        print("I am case 1");
+      }
+      break;
+    case "case 2":
+      {
+        print("I am case 2");
+      }
+      break;
+    case "case 3":
+      {
+        print("I am case 3");
+      }
+      break;
+    default:
+      {
+        print("I am default case");
+      }
+  }
+}
