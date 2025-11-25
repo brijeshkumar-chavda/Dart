ce3943a042ec106e1b34e3b66a298f5961a837d8
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-25 18:17:28 +0530

    refactor: list

--- 14_list/01_list.dart
+++ 14_list/01_list.dart
@@ -4,6 +4,11 @@
 void main() {
   // Different ways to create a list
   List<int> list1 = [1, 2, 3, 4, 5];
+  print("number list: $list1");
+
   List<String> list2 = ["element1", "element2", "element3"];
+  print("string list: $list2");
+
   var list3 = [1, "element2", true];
+  print("dynamic list: $list3");
 }
