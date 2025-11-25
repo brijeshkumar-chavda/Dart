fb9c5debb6137f95cbf4888518ad06073d14fd9a
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-25 18:33:21 +0530

    dart /home/kubuntu/Documents/brijesh/projects/Dart/14_list/property/length.dart

---
+++ 14_list/method/remove.dart
@@ -0,0 +1,16 @@
+// .remove()
+
+// What
+// - It takes one argument as a parameter.
+// - The value of the argument must be the element that you want to remove from the list.
+
+// Syntax
+// list.remove();
+
+// Example
+void main() {
+  var list = [1, 2, 3, 4, 5];
+
+  list.remove(1);
+  print(list);
+}
