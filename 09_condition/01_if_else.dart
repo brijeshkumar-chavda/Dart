42ff878d6cb357b60b34088e8651d25fff4d3519
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-25 14:00:39 +0530

    add: if else statement

---
+++ 09_condition/01_if_else.dart
@@ -0,0 +1,10 @@
+// If else conditional statement
+
+void main() {
+  bool condition = true;
+  if (condition) {
+    print("if block executed");
+  } else {
+    print("else block executed");
+  }
+}
