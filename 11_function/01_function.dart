d25017c9dd84369ef59f58459fb509140ecddd94
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-25 15:33:41 +0530

    add: function

---
+++ 11_function/01_function.dart
@@ -0,0 +1,17 @@
+// Function
+
+// What
+// - A function is a block of code that performs a specific task.
+// - It helps in code reusability and modularity.
+// - Functions can take input parameters and can return a value.
+
+// Syntax
+// returnType functionName(parameter1, parameter2, ...) {
+//   // Function body
+//   return value;
+// }
+
+String functionName(parameter) {
+  print("I am function");
+  return parameter;
+}
