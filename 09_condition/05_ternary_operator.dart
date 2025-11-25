ec01e5569b8a181e82cb6c5e4c4ecd2c66e1177a
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-25 14:42:54 +0530

    add: ternary operator

---
+++ 09_condition/05_ternary_operator.dart
@@ -0,0 +1,11 @@
+// Ternary Operator
+
+// Syntax
+// condition ? expression1 if true : expression2 if false
+
+// Example
+void main() {
+  bool condition = true;
+  String result = condition ? "Condition is True" : "Condition is False";
+  print(result); // Output: Condition is True
+}
