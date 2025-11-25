ae3f23f7b096b4bf2d25fd51196c018214c9cfc8
brijeshkumar-chavda <chavdabrijeshkumar25@gmail.com>
2025-11-25 18:28:30 +0530

    add: .add()

---
+++ 14_list/04_list_operations.dart
@@ -0,0 +1,15 @@
+// List operations
+
+// Example
+void main() {
+  var list = [10, 20, 30, 40, 50];
+
+  // Accessing the element using the index number
+  print(list[0]);
+
+  list.add(60);
+  print(list);
+
+  list.remove(10);
+  print(list);
+}
---
+++ 14_list/method/add.dart
@@ -0,0 +1,14 @@
+// .add()
+
+// What
+// - It's use add element to the list.
+// - It will add the new element at the end of the list.
+// - It will modified the original list.
+
+// Example
+void main() {
+  var list = [1, 2, 3, 4, 5];
+
+  list.add(6);
+  print(list);
+}
